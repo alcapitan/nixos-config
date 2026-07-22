d04e5095dc3f7599b77619758fa6945d622e29ba
Al Capitan <alexandre.boyer29@gmail.com>
2026-07-22 05:57:15 +0200

    feat: setup local dns server on pc
    - With cache, accelerating resolutions
    - With local overwriting
    - Use cloudflare instead of ISP

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -50,10 +50,6 @@
     wl-clipboard
   ];
 
-  networking.extraHosts = ''
-    127.1.0.1   alcapitan.local
-  '';
-
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
@@ -64,4 +60,35 @@
     enable = true;
     openFirewall = true;
   };
+
+  services.unbound = {
+    enable = true;
+    settings = {
+      server = {
+        local-zone = [ "\"alcapitan.local.\" redirect" ];
+        local-data = [
+          "\"alcapitan.local. IN A 127.1.0.1\""
+        ];
+
+        cache-min-ttl = 3600;          # Garde en mémoire minimum 1h quand c'est possible
+        cache-max-ttl = 86400;         # Cache maximum de 24h
+        prefetch = true;               # Rafraîchit automatiquement les domaines populaires avant qu'ils n'expirent
+        prefetch-key = true;           # Accélère les requêtes DNSSEC
+        msg-cache-size = "16m";        # Taille du cache des messages
+        rrset-cache-size = "32m";      # Taille du cache des enregistrements
+      };
+
+      forward-zone = [
+        {
+          name = ".";
+          forward-tls-upstream = true;
+          forward-addr = [
+            "1.1.1.1@853#cloudflare-dns.com"
+            "1.0.0.1@853#cloudflare-dns.com"
+          ];
+        }
+      ];
+    };
+  };
+  networking.nameservers = [ "127.0.0.1" ]; #! attention un wifi restreignant fermement les ports peut bloquer le dns, donc commenter cette ligne si besoin
 }
