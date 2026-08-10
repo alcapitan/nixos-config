2045b94c7766187ba1541b21a48eb8f231c82779
Al Capitan <alexandre.boyer29@gmail.com>
2026-08-11 00:34:28 +0200

    fix: borg systemd service didnt start automatically and also sent wrong notification status

--- hosts/laptop/borg.nix
+++ hosts/laptop/borg.nix
@@ -1,62 +1,78 @@
 { config, pkgs, ... }:
 
 {
-  systemd.services."borgbackup-job-backup".unitConfig.OnFailure = "borg-failure-notify.service";
+    services.borgbackup.jobs."backup" = {
+        repo = "ssh://u502181-sub4@u502181-sub4.your-storagebox.de:23/./pc";
+        doInit = false;
+        environment.BORG_RSH = "ssh -i /var/lib/secrets/borg-ssh-key";
+        extraCreateArgs = "--verbose --stats --exclude-from /home/alex/.borg-exclude";
+        paths = [ 
+            "/home"
+            "/var/lib/secrets" 
+        ];
+        failOnWarnings = false;
+        encryption = {
+            mode = "repokey-blake2";
+            passCommand = "cat /var/lib/secrets/borg-repo-password";
+        };
+        compression = "auto,zstd,6";
+        startAt = "daily";
 
-  systemd.services."borg-failure-notify" = {
-    description = "Notification graphique d'échec Borg";
-    script = ''
-      # On force l'exécution dans le contexte de ta session utilisateur graphique (ID 1000)
-      /run/current-system/sw/bin/systemd-run --user -M alex@ --collect \
-        ${pkgs.libnotify}/bin/notify-send --urgency=critical "❌ Échec du Backup Borg" "Le job de sauvegarde a planté. Vérifie les logs avec journalctl."
-    '';
-  };
-
-  services.borgbackup.jobs."backup" = {
-    repo = "ssh://u502181-sub4@u502181-sub4.your-storagebox.de:23/./pc";
-
-    doInit = false;
-
-    environment.BORG_RSH = "ssh -i /var/lib/secrets/borg-ssh-key";
-    extraCreateArgs = "--verbose --stats --exclude-from /home/alex/.borg-exclude";
-
-    paths = [
-      "/home"
-      "/var/lib/secrets"
-    ];
-
-    failOnWarnings = false;
-
-    encryption = {
-      mode = "repokey-blake2";
-      passCommand = "cat /var/lib/secrets/borg-repo-password";
+        prune = {
+            keep = {
+                within = "1d";
+                daily = 7;
+                weekly = 4;
+                monthly = 6;
+            };
+        };
+        extraPruneArgs = "--verbose --list";
     };
 
-    compression = "auto,zstd,6";
-    startAt = "daily";
+    systemd.services."borgbackup-job-backup" = {
+        after = [ "network-online.target" ];
+        wants = [ "network-online.target" ];
 
-    postHook = ''
-      /run/current-system/sw/bin/systemd-run --user -M alex@ --collect \
-        ${pkgs.libnotify}/bin/notify-send --urgency=normal "✅ Backup Borg Réussi" "Toutes tes données sont à l'abri sur Hetzner."
-    '';
+        onFailure = [ "borgbackup-job-backup-failure-notify.service" ];
 
-    prune = {
-      keep = {
-        within = "1d";
-        daily = 7;
-        weekly = 4;
-        monthly = 6;
-      };
+        unitConfig = {
+            OnSuccess = [ "borgbackup-job-backup-success-notify.service" ];
+        };
+
+        serviceConfig = {
+            Restart = "on-failure";
+            RestartSec = "5min";
+        };
+
+        restartIfChanged = false;
+        stopIfChanged = false;
     };
-    extraPruneArgs = "--verbose --list";
-  };
 
-  systemd.services."borgbackup-job-backup" = {
-    requires = [ "network-online.target" ];
+    systemd.services."borgbackup-job-backup-success-notify" = {
+        description = "Notify user on borg backup success";
+        serviceConfig = {
+        Type = "oneshot";
+        ExecStart = ''
+            /run/current-system/sw/bin/systemd-run --user -M alex@ --collect \
+            ${pkgs.libnotify}/bin/notify-send --urgency=normal "✅ Sauvegarde Borg réussi" "borg list borg-repo:pc"
+        '';
+        };
 
-    serviceConfig = {
-      Restart = "on-failure";
-      RestartSec = "1m";
+        restartIfChanged = false;
+        stopIfChanged = false;
     };
-  };
+
+    systemd.services."borgbackup-job-backup-failure-notify" = {
+        description = "Notify user on borg backup failure";
+        serviceConfig = {
+            Type = "oneshot";
+            ExecStart = ''
+                /run/current-system/sw/bin/systemd-run --user -M alex@ --collect \
+                ${pkgs.libnotify}/bin/notify-send --urgency=critical "❌ Échec de la sauvegarde Borg" "journalctl -u borgbackup-job-backup"
+            '';
+        };
+
+        restartIfChanged = false;
+        stopIfChanged = false;
+    };
 }
