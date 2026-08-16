6184af2b4def4690cdf661e51d02176b6738c3ca
Al Capitan <alexandre.boyer29@gmail.com>
2026-08-16 18:06:12 +0200

    refactor(vps): move virtualisation config to `test_env.nix`

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     # ./hardware-configuration.nix
+    ./test_env.nix
     ./services.nix
     # ./borg.nix
   ];
@@ -13,25 +14,6 @@
   
   console.earlySetup = true;
   boot.kernelParams = [ "numlock=on" ];
-
-  virtualisation.vmVariant = {
-    virtualisation = {
-      memorySize = 4096; # 4 GB RAM
-      cores = 2;         # 2 vCPU
-      diskSize = 40900;  # 40 GB Disk
-
-      # graphics = false;
-
-      forwardPorts = [
-        { from = "host"; host.address = "127.1.0.1"; host.port = 8000; guest.port = 80; }
-        { from = "host"; host.address = "127.1.0.1"; host.port = 8443; guest.port = 443; }
-        { from = "host"; host.address = "127.1.0.1"; host.port = 2222; guest.port = 2222; }
-        { from = "host"; host.address = "127.1.0.1"; host.port = 2223; guest.port = 22; }
-        { proto = "tcp"; from = "host"; host.address = "127.1.0.1"; host.port = 5353; guest.port = 53; }
-        { proto = "udp"; from = "host"; host.address = "127.1.0.1"; host.port = 5353; guest.port = 53; }
-      ];
-    };
-  };
 
   networking.firewall = {
     enable = true;
